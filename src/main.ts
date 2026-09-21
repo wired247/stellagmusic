1eadddaeca114b922c9e210e582083a772e71a33
David Willoughby <willoughby.d@gmail.com>
2026-09-21 08:55:17 -0400

    Add static Squarespace CSS globally

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,14 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
+// Import Squarespace static CSS
+import './assets/sqsp/universal/user-account-core-min.en-US.css'
+import './assets/sqsp/definitions/website-component-definition/static-assets/socialLinks/website.components.socialLinks.styles.css'
+import './assets/sqsp/definitions/website-component-definition/static-assets/imageFluid/website.components.imageFluid.styles.css'
+import './assets/sqsp/definitions/website-component-definition/static-assets/code/website.components.code.styles.css'
+import './assets/sqsp/definitions/website-component-definition/static-assets/html/website.components.html.styles.css'
+import './assets/sqsp/versioned-site-css/site.css'
+import './assets/sqsp/vta/versioned-assets/static_home.css'
+import './assets/sqsp/custom-css/custom.css'
+
 createApp(App).use(router).mount('#app')
